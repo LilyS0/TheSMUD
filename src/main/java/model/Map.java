a2edb1ad4fd8c0856f6e244fc8efb27228803b08
slw8658 <slw8658@rit.edu>
2023-10-17 13:43:24 -0400

    added general structure for Map and Room, as well as comments/examples to aid in further development

---
+++ src/main/java/model/Map.java
@@ -0,0 +1,20 @@
+/**
+ * A Map is made up of 2+ Rooms, which are made of Tiles.
+ * 
+ * @author Sydney Wilson
+ */
+public class Map {
+    // public Room startRoom;
+    // public Room endRoom;
+    // public Room[][] rooms;
+    public long time = System.currentTimeMillis(); // there might also be a better way to go about this, but this way is reliable at least
+
+    // *again with the 2D array, there's gotta be a better solution but for now that's
+    // gonna be the placeholder*
+
+    public boolean isNight;
+
+    public void timeChange() {
+        // if this.time - other time = 5 min or whatever then change time
+    }
+}
---
+++ src/main/java/model/Room.java
@@ -0,0 +1,23 @@
+/**
+ * A Map consists of many Rooms, each with varying attributes. A Room is then made of Tiles.
+ * 
+ * @author Sydney Wilson
+ */
+public class Room {
+    // *structure of this V E R Y subject to change, for now a 2D 
+    // array seems like a sort of decent solution similar to how you would create ex. a checkerboard
+    // for a game of checkers, but there is probably a much more optimal solution i have forgotten*
+
+    // public Tile[][] tiles;
+    
+    // width and height in terms of Tiles would then be the size of the array
+
+    // Exit is not currently a type of Tile in our ConsensusDomainModel, but i think it 
+    // would be optimal to do so (will further discuss in meeting)
+
+    public String description;
+
+    public String getDescription() {
+        return this.description;
+    }
+}
