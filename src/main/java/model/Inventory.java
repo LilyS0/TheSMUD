de722329c3bcb9259e3a078c7366963253e70860
LilyS0 <wds6609@rit.edu>
2023-10-17 16:41:24 -0400

    combining branch

--- src/main/java/model/Inventory.java
+++ src/main/java/model/Inventory.java
@@ -2,6 +2,8 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.Item.Item;
+
 /**
  * Inventory uses the Composite Design Pattern to create a part-whole hierarchy of Items to be stored for use 
  * by the PlayerCharacter or NPC.
@@ -20,4 +22,10 @@ public class Inventory implements Item {
     // Item types vary (seen in Item branch), but all implement Item interface, Inventory also implements
     // Item interface, and manages an ArrayList
     // ex. inventory.add(new Item(*armor*))
+
+    @Override
+    public int getStat() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getStat'");
+    }
 }
