12798ae33ade07e1f995f75cf887a50f940d725e
Jack <jdm8851@g.rit.edu>
2023-10-17 15:44:51 -0400

    Inventory Controller Created

---
+++ src/main/java/controller/UseInvKey.java
@@ -0,0 +1,5 @@
+package controller;
+
+public class UseInvKey implements useKey{
+    
+}
---
+++ src/main/java/controller/VisitMUDInventory.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class VisitMUDInventory implements VisitInv{
+    public static void visit() {
+        
+    }
+}
