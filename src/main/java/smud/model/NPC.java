620a69f805933db4ec888c7a8ee6e01e513e8753
slw8658 <slw8658@rit.edu>
2023-10-22 23:29:00 -0400

    y'all intellij sucks and i cannot tell if this works but if it does i added the random NPCs and the overload constructor

--- src/main/java/smud/model/NPC.java
+++ src/main/java/smud/model/NPC.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import smud.model.Item.*;
 
 /**
- * NPC implements the Character class amnd inherits the functionality associated with it, and adds
+ * NPC implements the Character class and inherits the functionality associated with it, and adds
  * additional information associated with the NPC such as nocturnal/diurnal.
  * 
  * @author Sydney Wilson
@@ -25,7 +25,22 @@ public class NPC extends MUDCharacter{
 // method graveyard: setHealth
 
     private boolean isNocturnal;
-     
+    private String[][] randomNPCs = {{"The Merchant", "Everyone's favorite street pharmacist."},
+    {"Sans", "A metaphysics expert, knowledgeable in the ways of space-time manipulation and the multiverse."},
+    {"Tom Nook", "Love him or hate him, either way you probably owe him."},
+    {"Navi", "Sometimes the best advice, is no advice."},
+    {"Igor", "Your friendly neighborhood blacksmith. Or is he?"},
+    {"Count Chocula", "Not to be confused with the Count von Count."},
+    {"Justin Case", "Well-known lawyer specializes in defending Goats and Trolls."},
+    {"Guy DeFault", "Guy Fieri's half cousin twice removed."},
+    {"Cosmic Menace", ""},
+    {"Alex", "His dad works for Matlab. You've been warned."},
+    {"Bobert St. Jaqass", "He looked at me funny, and even though I have no idea why, it offended me anyway."},
+    {"Bubert", "When I actually went to class, halfway through I began to hate God for giving me the legs that brought me there."},
+    {"Rotisserie St. Jacks", "Enrolling in GCIS-LRATIO has been the biggest mistake of my entire life. I don't even know what a capital letter looks like anymore."},
+    {"Hank Schmogus", ""},
+    {"Paul Creenis", ""}};
+    Random rand = new Random();
     public boolean getNocturnal() {
         return this.isNocturnal;
     }
@@ -37,7 +52,6 @@ public class NPC extends MUDCharacter{
     public NPC(String name, String description, Inventory inventory, boolean isNocturnal) {
         this.setName(name);
         this.setDescription(description);
-        Random rand = new Random();
         int minHealth = 50;
         int maxHealth = 150;
         this.health = rand.nextInt(maxHealth - minHealth + 1) + minHealth;
@@ -51,11 +65,24 @@ public class NPC extends MUDCharacter{
         return null;
     }
 
+    public NPC() {
+        int minHealth = 50;
+        int maxHealth = 150;
+        this.health = rand.nextInt(maxHealth - minHealth + 1) + minHealth;
+        int NPCnumber = rand.nextInt(15); //15 total options (located 0-14 in array)
+        this.name = randomNPCs[NPCnumber][0];
+        this.description = randomNPCs[NPCnumber][1];
+    }
+
+//     public static void main(String[] args) {
+// //        Inventory inv = new Inventory();
+// //        NPC alex = new NPC("Alex", "This is NPC Alex", inv, false);
+// //        System.out.println(alex.getName());
+// //        System.out.println(alex.getNocturnal());
+// //        System.out.println(alex.getHealth());
+//         System.out.println(new NPC());
+//     }
     public static void main(String[] args) {
-        Inventory inv = new Inventory();
-        NPC alex = new NPC("Alex", "This is NPC Alex", inv, false);
-        System.out.println(alex.getName());
-        System.out.println(alex.getNocturnal());
-        System.out.println(alex.getHealth());
+        System.out.println(new NPC());
     }
 }
