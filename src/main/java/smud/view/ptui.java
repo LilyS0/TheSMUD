d06e2a1be48f03476fd91190aa9eeb46e4120e65
LilyS0 <wds6609@rit.edu>
2023-10-23 20:29:56 -0400

    humble beginnings

--- src/main/java/smud/view/ptui.java
+++ src/main/java/smud/view/ptui.java
@@ -1,5 +1,15 @@
 package smud.view;
 
+import java.io.IOException;
+
+import smud.model.Environment.SMUDMap;
+
 public class ptui {
     
+    private SMUDMap map;
+
+
+    public ptui() throws IOException{
+        this.map = new SMUDMap("src/main/java/smud/maps/map1.txt");
+    }
 }
