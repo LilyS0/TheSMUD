c810d1ebfa6130dc2b22be21bbbc46b7af975676
Ty Platow <70657397+tplatow64@users.noreply.github.com>
2023-11-30 11:50:39 -0500

    mainly adding pseudocode and what we still need for merchant

--- src/main/java/smud/model/Environment/Tiles/MerchantTile.java
+++ src/main/java/smud/model/Environment/Tiles/MerchantTile.java
@@ -5,13 +5,29 @@ import smud.model.MUDException;
 import smud.model.Character.MUDCharacter;
 import smud.model.Item.MUDItem;
 
+/*
+ * WHAT WE STILL NEED:
+ *  IN OTHER FILES:
+ *  WAY TO SET MERCHANT IN A ROOM TO CLOSED WHEN ROOM NOT CLEARED
+ *  WAY TO SET MERCHANTS CLOSED WHEN NIGHT TIME
+ *  WAY TO SET MERCHANTS OPEN WHEN CLEARED + DAY TIME
+ *  IN THIS FILE:
+ *  INTERACT METHOD, READING INPUT AND PASS TO BUYITEM
+ *      WHILE LOOP, INPUT == exit TO BREAK
+ *      ELSE PASS INPUT TO BUYITEM, IF IT THROWS EXCEPTION HANDLE IT
+ */
+
 public class MerchantTile extends TileFeature{
     private MUDItem[] forSale;
     private boolean open;
     
-    public MerchantTile(){
-        forSale = new MUDItem[]{MUDItem.getRandomItem(), MUDItem.getRandomItem(), MUDItem.getRandomItem()};
-        open = false;
+    public MerchantTile(int x, int y){
+        this.forSale = new MUDItem[]{MUDItem.getRandomItem(), MUDItem.getRandomItem(), MUDItem.getRandomItem()};
+        this.open = false;
+        this.defaultSymbol = 'M';
+        this.symbol = defaultSymbol;
+        this.xCor = x;
+        this.yCor = y;
     }
 
     public boolean isOpen(){
@@ -43,6 +59,17 @@ public class MerchantTile extends TileFeature{
     public void interact(PlayerController player) throws MUDException {
         if(open){
             System.out.println("You have " + player.getCharacter().getGold() + " gold.");
+            for(int i=0; i<forSale.length; i++){
+                System.out.println(i + ": " + forSale[i] + " - " + forSale[i].getValue() + " gold"); //e.g. 0: Sword - 25 gold
+            }
+            /*
+             * read an input
+             * try{
+             *      buyItem(input);
+             * }catch(MUDException e){
+             *      System.out.println("Invalid item. Please enter 0, 1, or 2.");
+             * }
+             */
         }else{
             System.out.println("The merchant is closed.");
         }
