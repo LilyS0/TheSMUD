31d00b10448fd127a8de9c1bb4170c19bea56c54
LilyS0 <wds6609@rit.edu>
2023-12-27 17:55:17 -0500

    working web-page

--- src/main/java/smud/view/smud-gui/src/App.js
+++ src/main/java/smud/view/smud-gui/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-import Homepage from './Homepage';
-import PlayChoice from './PlayChoice';
+import Homepage from './pages/Homepage';
+import PlayChoice from './pages/PlayChoice';
+import Store from './pages/Store';
+import Inventory from './pages/Inventory';
 import React from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   
@@ -10,12 +12,13 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage/>} />
+          <Route index element={<Homepage/>} />
           <Route path="/play-choice" element={<PlayChoice/>} />
+          <Route path="/store" element={<Store/>}/>
+          <Route path="/inventory" element={<Inventory/>}/>
         </Routes>
       </BrowserRouter>
     </div>
-    
   );
 }
 
--- src/main/java/smud/view/smud-gui/src/pages/PlayChoice.js
+++ src/main/java/smud/view/smud-gui/src/pages/PlayChoice.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function PlayChoice(){
     return(
         <div>
-            <h1>Dungeon</h1> <h1>Arena</h1>
+            <h1>Dungeon</h1> 
+            <h1>Arena</h1>
         </div>
     )
 }
