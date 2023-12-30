850b39a91c3b12446427a29e5c88f3adcd4a8ad7
LilyS0 <wds6609@rit.edu>
2023-12-30 02:22:46 -0500

    first half of angular tutorial

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HeroesComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
